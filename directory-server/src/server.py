d3e05f4a8336e16cb363e5e4c2163247db169f0b
¨RodrigoCSoares¨ <soares.c.rodrigo@gmail.com>
2019-04-07 22:38:20 -0300

    API test implementation

--- directory-server/src/server.py
+++ directory-server/src/server.py
@@ -1,22 +1,82 @@
-from flask import (
-    Flask,
-    render_template
-)
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
+api = Api(app)
 
-# Create a URL route in our application for "/"
-@app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
+# Create the database
+users = [
+    {
+        "name": "Rodrigo",
+        "age": 19,
+        "occupation": "Software Engineer"
+    },
+    {
+        "name": "Angel",
+        "age": 32,
+        "occupation": "Singer"
+    },
+    {
+        "name": "Jessica",
+        "age": 43,
+        "occupation": "Developer"
+    }
+]
 
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('home.html')
 
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
-    app.run(debug=True)
+# API endpoints
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if name == user["name"]:
+                return user, 200
+            return "User not found", 404
+
+    def post(sel, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if name == user["name"]:
+                return "User with {} already exists".format(name), 400
+
+            user = {
+                "name": name,
+                "age": args["age"],
+                "occupation": args["occupation"]
+            }
+            users.append(user)
+            return user, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if name == user["name"]:
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted.".format(name), 200
+
+
+# Add api resource route
+api.add_resource(User, "/user/<string:name>")
+app.run(debug=True)
