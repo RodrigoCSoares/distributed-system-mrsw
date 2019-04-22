6157288bf59dcfeda4091688fa7399fe30207c06
¨RodrigoCSoares¨ <soares.c.rodrigo@gmail.com>
2019-04-21 21:45:44 -0300

    directory-server: Added chunk servers registration functionality

---
+++ directory-server/src/chunk_server.py
@@ -0,0 +1,44 @@
+from flask_restful import Resource, reqparse
+
+# Create the database
+chunkServers = []
+
+
+# API endpoints
+class ChunkServer(Resource):
+    def get(self, ip):
+        parser = reqparse.RequestParser()
+        parser.add_argument("port")
+        args = parser.parse_args()
+
+        for chunk_server in chunkServers:
+            if ip == chunk_server["ip"] and args["port"] == chunk_server["port"]:
+                return chunk_server, 200
+
+        return "Chunk server not found", 404
+
+    def post(self, ip):
+        parser = reqparse.RequestParser()
+        parser.add_argument("port")
+        args = parser.parse_args()
+
+        for chunkServer in chunkServers:
+            if ip == chunkServer["ip"] and args["port"] == chunkServer["port"]:
+                return "Chunk server with ip {} and port {} already exists".format(ip, chunkServer["port"]), 400
+
+        new_chunk_server = {
+            "ip": ip,
+            "port": args["port"]
+        }
+        chunkServers.append(new_chunk_server)
+        return new_chunk_server, 201
+
+    def delete(self, ip):
+        global chunkServers
+        parser = reqparse.RequestParser()
+        parser.add_argument("port")
+        args = parser.parse_args()
+
+        chunkServers = [ChunkServer for ChunkServer in chunkServers if
+                        ChunkServer["ip"] != ip and ChunkServer["port"] != args["port"]]
+        return "Chunk server with ip {} and port {} is deleted.".format(ip, args["port"]), 200
--- directory-server/src/server.py
+++ directory-server/src/server.py
@@ -1,66 +1,12 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api
+from chunk_server import ChunkServer
 
 # Create the application instance
 app = Flask(__name__)
 api = Api(app)
-
-# Create the database
-users = []
-
-# API endpoints
-class User(Resource):
-    def get(self, name):
-        for user in users:
-            if name == user["name"]:
-                return user, 200
-
-        return "User not found", 404
-
-    def post(sel, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if name == user["name"]:
-                return "User with {} already exists".format(name), 400
-
-            user = {
-                "name": name,
-                "age": args["age"],
-                "occupation": args["occupation"]
-            }
-            users.append(user)
-            return user, 201
-
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if name == user["name"]:
-                user["age"] = args["age"]
-                user["occupation"] = args["occupation"]
-                return user, 200
-
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
-
-    def delete(self, name):
-        global users
-        users = [user for user in users if user["name"] != name]
-        return "{} is deleted.".format(name), 200
-
+chunk = ChunkServer
 
 # Add api resource route
-api.add_resource(User, "/user/<string:name>")
+api.add_resource(chunk, "/chunk_server/<string:ip>")
 app.run(debug=True)
